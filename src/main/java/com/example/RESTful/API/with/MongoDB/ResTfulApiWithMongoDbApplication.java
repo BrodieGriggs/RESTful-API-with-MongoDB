7b54bcb062b42896584e23cc84e4c92caca6d04c
Brodie Griggs <48910025+BrodieGriggs@users.noreply.github.com>
2022-09-27 09:36:43 +0100

    Initial Project Files Generated using Spring Initializr

---
+++ src/main/java/com/example/RESTful/API/with/MongoDB/ResTfulApiWithMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.RESTful.API.with.MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulApiWithMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulApiWithMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RESTful/API/with/MongoDB/ResTfulApiWithMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RESTful.API.with.MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiWithMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

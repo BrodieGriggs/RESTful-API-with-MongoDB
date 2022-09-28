fb4ce069b204d12e7945dc75e5d36d6458241075
Brodie Griggs <48910025+BrodieGriggs@users.noreply.github.com>
2022-09-28 18:14:56 +0100

    Set Up ajax request to get data from the tomcat server

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -3,8 +3,43 @@ import React from "react";
 import './App.css';
 
 const App = () => {
+
+    const [customerList, setCustomerList] = React.useState([undefined]);
+
+    const getCustomerList = () => {
+        $.ajax({
+            url: "http://localhost:8080/customers",
+            type: "GET",
+            crossDomain: true,
+            crossOrigin: true,
+            error: (xhr, status, errorThrown) => {
+                alert(status, errorThrown);
+                console.log("xhr: " + xhr);
+                console.log("status: " + status);
+                console.log("errorThrown: " + errorThrown);
+            }
+        }).then( data => {
+            setCustomerList(data);
+        })
+    }
+    
+    React.useEffect(() => {
+        if(customerList[0] === undefined) { getCustomerList(); } 
+    })
+
     return (
         <div>
+            {customerList.map((customer, idx) => {
+                if(customer === undefined) { return; }
+                return (
+                    <div id="customer" key={idx}>
+                        <h3>Customer {idx + 1}</h3>
+                        <p>id: {customer.id}</p>
+                        <p>First Name: {customer.firstName}</p>
+                        <p>Last Name: {customer.lastName}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
